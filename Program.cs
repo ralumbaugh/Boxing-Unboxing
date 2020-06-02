2d2f2cac46a043c4d6444403c9fbb1450be2efe6
Ryan Alumbaugh <ralumbaugh@gmail.com>
2020-06-01 23:21:19 -0700

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BoxingUnboxing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<object> ObjectList = new List<object>();
+            ObjectList.Add(7);
+            ObjectList.Add(28);
+            ObjectList.Add(-1);
+            ObjectList.Add(true);
+            ObjectList.Add("chair");
+            int MySum = 0;
+            for(int i=0; i<ObjectList.Count; i++)
+            {
+                System.Console.WriteLine(ObjectList[i]);
+                if(ObjectList[i] is int)
+                {
+                    MySum = MySum+(int)ObjectList[i];
+                }
+            }
+            System.Console.WriteLine(MySum);
+        }
+    }
+}
